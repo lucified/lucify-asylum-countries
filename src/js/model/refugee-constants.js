41aeec2d840fd530ff07440d0161baaf08098861
Ville Saarinen <ville.saarinen@gmail.com>
2016-01-27 12:12:03 +0200

    Add start data moment to RefugeeConstants

--- src/js/model/refugee-constants.js
+++ src/js/model/refugee-constants.js
@@ -1,14 +1,16 @@
 
 var moment = require('moment');
 
-// note that month indices are zero-based
+// NOTE: month indices are zero-based
 
 module.exports.DATA_START_YEAR = 2012;
 module.exports.DATA_START_MONTH = 0;
+module.exports.DATA_START_MOMENT = moment([
+  module.exports.DATA_START_YEAR,
+  module.exports.DATA_START_MONTH]).startOf('month');
 
 module.exports.DATA_END_YEAR = 2015;
 module.exports.DATA_END_MONTH = 10;
-
 module.exports.DATA_END_MOMENT = moment([
 	module.exports.DATA_END_YEAR,
 	module.exports.DATA_END_MONTH]).endOf('month');
