e9206656c3e0ed56e6e69009d8e98fc3d2797fe9
Ville Väänänen <ville.vaananen@lucify.com>
2016-01-27 13:54:03 +0200

    Deploy each commit to a separate folder

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,15 @@
 
 var gulp = require('gulp');
 
+var path = (process.env.PROJECT || 'vnk-asylum-countries')
+path += process.env.COMMIT ? '-' + process.env.COMMIT.substr(0, 7) : ''
+
 var opts = {
 	paths: ['node_modules/lucify-commons'],
 	publishFromFolder: 'dist',
 	defaultBucket: 'lucify-dev',
 	maxAge: 3600,
-	assetContext: 'embed/the-flow-towards-europe-updating/',
+	assetContext: 'embed/'+path,
 	baseUrl: 'http://www.lucify.com/'
 }
 
