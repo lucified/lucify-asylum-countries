43fa2aac34085ab330fed1a3f395cbb5d1d5eda7
Juho Ojala <juho@lucify.com>
2016-01-28 13:41:45 +0000

    Add function for calculating time range in months

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,6 @@
 
+var moment = require('moment');
+
 function daysInMonth(month,year) {
   return new Date(year, month + 1, 0).getDate();
 }
@@ -7,5 +9,10 @@ function kmhToDegsPerH(kmh) {
   return kmh / 111;
 }
 
+function timeRangeInMonths(timeRange) {
+  return Math.round(moment(timeRange[1]*1000).diff(moment(timeRange[0]*1000), 'months', true));
+}
+
 module.exports.daysInMonth = daysInMonth;
 module.exports.kmhToDegsPerH = kmhToDegsPerH;
+module.exports.timeRangeInMonths = timeRangeInMonths;
