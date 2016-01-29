ca88689d920ef545752eaf9bbc60e024fb609365
Ville Saarinen <ville.saarinen@gmail.com>
2016-01-29 09:34:03 +0200

    Use Finnish country names

--- src/js/model/map-model.js
+++ src/js/model/map-model.js
@@ -74,14 +74,15 @@ MapModel.prototype.getLabelPointForCountry = function(country) {
 
 MapModel.prototype.getFriendlyNameForCountry = function(country) {
   switch(country) {
-    case "SYR": return "Syria";
-    case "MKD": return "Macedonia";
+    case "SYR": return "Syyria";
+    case "MKD": return "Makedonia";
     case "IRN": return "Iran";
     case "LBY": return "Libya";
-    case "RUS": return "Russia";
-    case "RCB": return "Congo";
-    case "COD": return "Congo";
-    default: return countries.getName(country, "en");
+    case "RUS": return "Venäjä";
+    case "RCB": return "Kongo";
+    case "COD": return "Kongo";
+    case "GBR": return "Iso-Britannia";
+    default: return countries.getName(country, "fi");
   }
 };
 
