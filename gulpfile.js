2e5864243db421f592318a863aaea39d71eaa6a6
Juho Ojala <juho@lucify.com>
2016-04-12 15:22:00 +0100

    Add support for development deploys to fixed URL in lucify-protected

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,19 @@ var gulp = require('gulp');
 var opts = {
   paths: ['node_modules/lucify-commons'],
   publishFromFolder: 'dist',
-  assetContext: 'vnk-asylum-countries/'
+  assetContext: 'vnk-asylum-countries/',
+
+  bucket: (env) => {
+    if (env == 'development') {
+      return 'lucify-protected';
+    }
+  },
+  baseUrl: (env) => {
+    if (env == 'development') {
+      return 'https://protected.lucify.com/';
+    }
+  }
+
 };
 
 var builder = require('lucify-component-builder');
