b50a3a5c060d389f8d0bc5090d6e144f73157f72
Juho Ojala <juho@lucify.com>
2016-05-04 01:32:44 +0300

    Adapt deploy configuration for production

--- lucify-opts.js
+++ lucify-opts.js
@@ -2,16 +2,5 @@
 module.exports = {
   paths: ['node_modules/lucify-commons'],
   publishFromFolder: 'dist',
-  assetContext: 'vnk-asylum-countries/',
-
-  bucket: (env) => {
-    if (env == 'development') {
-      return 'lucify-protected';
-    }
-  },
-  baseUrl: (env) => {
-    if (env == 'development') {
-      return 'https://protected.lucify.com/';
-    }
-  }
+  assetContext: 'embed/vnk-asylum-countries/'
 };
